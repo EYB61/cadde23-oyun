54df13cbc4ba5ed5e2bd4039393458615810530b
EYB61 <erdembozok6161@gmail.com>
2026-06-07 18:08:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,68 @@
 let socket;
-let roomCode = "";
-let playerName = "";
+let room;
+let name;
 
 function connect() {
-  playerName = document.getElementById("name").value;
+  name = document.getElementById("name").value;
+
   socket = io("https://YOUR-SERVER-URL");
 
   socket.on("roomJoined", (data) => {
-    roomCode = data.room;
+    room = data.room;
 
     document.getElementById("menu").style.display = "none";
     document.getElementById("game").style.display = "block";
 
-    document.getElementById("room").innerText = roomCode;
+    document.getElementById("room").innerText = room;
   });
 
   socket.on("updatePlayers", (players) => {
-    document.getElementById("players").innerText =
-      players.map(p => p.name).join(", ");
-  });
-
-  socket.on("chat", (data) => {
-    let div = document.createElement("div");
-    div.innerText = `${data.name}: ${data.msg}`;
-    document.getElementById("chat").appendChild(div);
+    document.getElementById("players").innerHTML =
+      players.map(p => `${p.name} ${p.alive ? "" : "(DEAD)"}`).join("<br>");
   });
 
   socket.on("status", (msg) => {
-    alert(msg);
+    document.getElementById("status").innerText = msg;
+  });
+
+  socket.on("turn", (player) => {
+    document.getElementById("status").innerText = "Turn: " + player;
+  });
+
+  socket.on("result", (data) => {
+    let div = document.createElement("div");
+    div.innerText = `${data.player} -> ${data.result}`;
+    document.getElementById("chat").appendChild(div);
+  });
+
+  socket.on("gameOver", (winner) => {
+    alert("Winner: " + winner);
   });
 }
 
 function createRoom() {
   connect();
-  socket.emit("createRoom", { name: playerName });
+  socket.emit("createRoom", { name });
 }
 
 function joinRoom() {
   connect();
-  let room = document.getElementById("roomCode").value;
-  socket.emit("joinRoom", { name: playerName, room });
-}
-
-function sendMsg() {
-  let msg = document.getElementById("msg").value;
-
-  socket.emit("chat", {
-    room: roomCode,
-    msg,
-    name: playerName
-  });
+  let r = document.getElementById("roomInput").value;
+  socket.emit("joinRoom", { name, room: r });
 }
 
 function startGame() {
-  socket.emit("startGame", roomCode);
+  socket.emit("startGame", room);
+}
+
+function playTurn() {
+  socket.emit("playTurn", { room });
+}
+
+function sendMsg() {
+  socket.emit("chat", {
+    room,
+    msg: document.getElementById("msg").value,
+    name
+  });
 }
