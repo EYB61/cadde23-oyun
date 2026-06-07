eea1036972a2bd10889a430fac143e14ae9cfc4d
EYB61 <erdembozok6161@gmail.com>
2026-06-07 18:06:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
+let socket;
+let roomCode = "";
 let playerName = "";
-let room = "lobby1";
 
-function joinGame() {
-  playerName = document.getElementById("nameInput").value;
+function connect() {
+  playerName = document.getElementById("name").value;
+  socket = io("https://YOUR-SERVER-URL");
 
-  if (!playerName) return alert("Enter name!");
+  socket.on("roomJoined", (data) => {
+    roomCode = data.room;
 
-  document.getElementById("login").style.display = "none";
-  document.getElementById("game").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("game").style.display = "block";
 
-  connectSocket(playerName, room);
+    document.getElementById("room").innerText = roomCode;
+  });
+
+  socket.on("updatePlayers", (players) => {
+    document.getElementById("players").innerText =
+      players.map(p => p.name).join(", ");
+  });
+
+  socket.on("chat", (data) => {
+    let div = document.createElement("div");
+    div.innerText = `${data.name}: ${data.msg}`;
+    document.getElementById("chat").appendChild(div);
+  });
+
+  socket.on("status", (msg) => {
+    alert(msg);
+  });
+}
+
+function createRoom() {
+  connect();
+  socket.emit("createRoom", { name: playerName });
+}
+
+function joinRoom() {
+  connect();
+  let room = document.getElementById("roomCode").value;
+  socket.emit("joinRoom", { name: playerName, room });
+}
+
+function sendMsg() {
+  let msg = document.getElementById("msg").value;
+
+  socket.emit("chat", {
+    room: roomCode,
+    msg,
+    name: playerName
+  });
+}
+
+function startGame() {
+  socket.emit("startGame", roomCode);
 }
